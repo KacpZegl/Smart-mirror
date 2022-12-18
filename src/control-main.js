571027eac4499a0065d7739473be4ab1183dbb59
KacpZegl <k.zeglen@gazeta.pl>
2022-12-18 19:56:23 +0100

    update

--- src/control-main.js
+++ src/control-main.js
@@ -3,13 +3,12 @@ var getWeather_time = 5; //in minutes
 var getForecast_time = 10; //in minutes
 var fullDate = new Date();
 var forecast_time = fullDate.getHours();
-const isOnline = window.navigator.onLine;
 
 getWeather_time = getWeather_time * 60 * 1000;
 getForecast_time = getForecast_time * 60 * 1000;
 
 function Update() {
-    
+    var isOnline = window.navigator.onLine;
     if (isOnline) {
         console.log('Online');
         getWeather();
@@ -22,5 +21,6 @@ function Update() {
     setInterval(getTime, 500);
     getDays();
 }
+setInterval(Update, 1000);
 
  
